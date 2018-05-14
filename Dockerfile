e0b358b66196979be131c62944b41f7f2c4e9c1e
Archis Gore <archis@polyverse.io>
2018-05-13 22:07:02 -0700

    Move build-php.sh into a separate script that can be run multiple times.

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM ubuntu
+
+COPY build-php.sh /php
+WORKDIR /php
+ENTRYPOINT ["/bin/bash", "/php/build-php.sh"]
---
+++ build-php.sh
@@ -0,0 +1,17 @@
+apt-get update && apt-get -y upgrade
+apt-get install -y \
+    git \
+    make \
+    autoconf \
+    gcc \
+    re2c \
+    bison \
+    libxml2-dev 
+
+git clone --depth 1 https://github.com/php/php-src.git
+cd php-src
+
+./buildconf
+./configure
+make
+
