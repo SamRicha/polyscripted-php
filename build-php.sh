626e6426dfdf25c9bc84b55270c27e03dcf23ed4
Archis Gore <archis@polyverse.io>
2018-05-15 09:34:22 -0700

    Make install to /polyscripted-php

--- build-php.sh
+++ build-php.sh
@@ -1,22 +1,16 @@
-apt-get update && apt-get -y upgrade
-apt-get install -y \
-    git \
-    make \
-    autoconf \
-    gcc \
-    re2c \
-    bison \
-    libxml2-dev \
-    vim
-
 git clone --depth 1 https://github.com/php/php-src.git
 cd php-src
 
 ./buildconf
-./configure --without-pear 
 
-sed 's/.*PHP_EXTRA_VERSION.*/PHP_EXTRA_VERSION = -polyscripted/' configure > configure.polyscripted
+sed 's/PHP_EXTRA_VERSION=.*/PHP_EXTRA_VERSION=-polyscripted/' configure > configure.polyscripted
 yes | cp configure.polyscripted configure
+
+./configure \
+  --without-pear \
+  --exec-prefix=/polyscripted-php \
+  --prefix=/polyscripted-php
+
 
 make
 
